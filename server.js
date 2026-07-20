4298b48752f1cde71a9e231ae69e5ab198945ff7
fardeenkhan112 <shahidkhan20201977@gmail.com>
2026-07-20 18:22:12 +0500

    Project completed

--- server.js
+++ server.js
@@ -2,19 +2,28 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
+// Connect Database
 connectDB();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
+app.use("/api/auth", authRoutes);
+
+// Home Route
 app.get("/", (req, res) => {
-  res.send("Backend Server Running...");
+  res.send("🚀 Backend Server Running...");
 });
 
+// Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
