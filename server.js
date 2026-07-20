4f48c47fbbdde5e9a0108bf9d918d3a1ff1cd973
fardeenkhan112 <shahidkhan20201977@gmail.com>
2026-07-20 17:16:45 +0500

    mongos connected

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+require("dotenv").config();
+
 const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
 
 const app = express();
 
-const PORT = 5000;
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend Server Running...");
+});
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`✅ Server is running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
